a79f5321fe33a28896e35c7b2599cd3ae8a45b1e
Zinhle <mcravemap@gmail.com>
2018-09-26 13:45:19 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,12 +3,79 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+
 })
 export class HomePage {
+  
+ name: "";
+ total: number;
+ showValue: number;
+ WhippedCream: boolean;
+ Chocolate: boolean;
+
+
+choc: number;
+Wh: number;
+WhChic: number;
+
+
+
+isIt: boolean;
 
   constructor(public navCtrl: NavController) {
+    this.total = 0;
+    this.isIt = false;
 
+    this.WhChic = 0;
+    this.Wh = 0;
+    this.choc = 0;
+  }
+  onClickAdd(){
+      this.total += 1;
+    
+      }
+    
+  
+    onClickSubtract(){
+      this.total -= 1;
+      
+   } 
+    onClickReset(){
+      this.total= 0;
+      this.showValue = 0;
+
+    }
+
+    onCheckNUM(){
+      if(this.total==0){
+        this.isIt = true;
+      }
+    }
+    showSum(){
+      this.showValue= 1;
+      this.totNumber();
+    }
+    totNumber(){
+
+      if(this.Chocolate == true && this.WhippedCream ==true){
+        this.WhChic +=1;
+      }else{
+        if (this.Chocolate){
+          this.choc += 1;
+        }
+        if(this.WhippedCream){
+          this.Wh +=1;
+        }   
+     
+   }
+   console.log(this.choc);
+   console.log(this.Wh);
+   console.log(this.WhChic);
+
+
+    }
+  
   }
 
-}
+  
